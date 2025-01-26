31ec8273519f33bdb6b28533765108c0c3c75a2b
despinamp <despina.bourou@gmail.com>
2025-01-26 14:42:58 -0500

    models (not completed)

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,11 +1,11 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarModel,CarMake
 
 
 # Register your models here.
 
-# CarModelInline class
-
+admin.site.register(CarModel)
+admin.site.register(CarMake)
 # CarModelAdmin class
 
 # CarMakeAdmin class with CarModelInline
