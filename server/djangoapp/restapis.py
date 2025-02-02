046a95f7d08754303e3c61ba5452e0131d3b2029
despinamp <despina.bourou@gmail.com>
2025-02-02 10:59:30 -0500

    create ddjango proxy services of backend apis

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -11,12 +11,38 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
-# def get_request(endpoint, **kwargs):
-# Add code for get requests to back end
+def get_request(endpoint, **kwargs):
+    params=""
+    if(kwargs):
+        for key,value in kwargs.items():
+            params=params+key+"="+value+"&"
+    request_url = backend_url+endpoint+"?"+params
+    print("GET from {}".format(request_url))
+    try:
+        #call get method of the requests library with url and parameters
+        response=requests.get(request_url)
+        return response.json()
+    except:
+        #if any error occurs
+        print("network exception occured")
 
-# def analyze_review_sentiments(text):
-# request_url = sentiment_analyzer_url+"analyze/"+text
-# Add code for retrieving sentiments
 
-# def post_review(data_dict):
-# Add code for posting review
+def analyze_review_sentiments(text):
+    request_url = sentiment_analyzer_url+"analyze/"+text
+    try:
+        #call get method of requests library with url and parameters
+        response = requests.get(request_url)
+        return response.json()
+    except Exception ass err:
+        print(f"Unexpected {err=},{type(err)=}")
+        print("Network exception occured")
+
+def post_review(data_dict):
+    request_url=backend_url+"/insert_review"
+    try:
+        response=requests.post(request_url,json=data_dict)
+        print(response.json())
+        return response.json()
+    except:
+        print("network exception occured")
+
