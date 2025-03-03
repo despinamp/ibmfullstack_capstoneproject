8a02fc25b661f0228a5ca7db06ddbebf51135e8c
despinamp <120451233+despinamp@users.noreply.github.com>
2025-03-03 20:32:31 +0200

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -31,10 +31,11 @@ DEBUG = True
 ALLOWED_HOSTS = ['localhost',
                  ('https://despinabouro-8000'
                      '.theiadockernext-1-labs-prod-theiak8s'
-                      '-4-tor01.proxy.cognitiveclass.ai/')]
+                     '-4-tor01.proxy.cognitiveclass.ai/')]
 CSRF_TRUSTED_ORIGINS = [
     ('https://despinabouro-8000'
-        '.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai')]
+        '.theiadockernext-1-labs-prod'
+        '-theiak8s-4-tor01.proxy.cognitiveclass.ai')]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
