0211300eea2273fd9f5cb7e7a837ef637f97744a
despinamp <120451233+despinamp@users.noreply.github.com>
2025-03-03 20:15:13 +0200

    Update views.py github actions lint error vol3

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -129,7 +129,7 @@ def get_dealer_reviews(request, dealer_id):
         for review_detail in reviews:
             response = analyze_review_sentiments(review_detail['review'])
             print(response)
-            review_detail['sentiment'] = response['sentiment']            
+            review_detail['sentiment'] = response['sentiment']        
         return JsonResponse({"status": 200, "reviews": reviews})
     else:
         return JsonResponse({"status": 400, "message": "Bad Request"})
@@ -144,7 +144,7 @@ def add_review(request):
             print(response)
             return JsonResponse({"status": 200})
         except Exception:
-            return JsonResponse({"status": 401, 
+            return JsonResponse({"status": 401,
                                  "message": "error in posting review"})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
