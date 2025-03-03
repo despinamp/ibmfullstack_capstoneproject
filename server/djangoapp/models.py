0759b746e3530901e1a871bd695a047073c64b23
despinamp <120451233+despinamp@users.noreply.github.com>
2025-03-03 20:12:52 +0200

    Update models.py github actions lint error vol3

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -15,9 +15,9 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    
-    def __str__(self):
-        return self.name
+
+def __str__(self):
+    return self.name
 
 
 class CarModel(models.Model):
