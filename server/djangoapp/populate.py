2af3b07963271d8d09078f16baebda7d8f285dec
despinamp <120451233+despinamp@users.noreply.github.com>
2025-03-03 19:58:06 +0200

    Update populate.py github actions lint error vol2

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -12,23 +12,24 @@ def initiate():
 
     car_make_instances = []
     for data in car_make_data:
-        car_make_instances.append(CarMake.objects.create(name=data['name'], 
-                                                         description=data['description']))
+        car_make_instances.append(
+            CarMake.objects.create(name=data['name'],
+            description=data['description']))
 
     # Create CarModel instances with the corresponding CarMake instances
     car_model_data = [
       {"name": "Pathfinder", "type": "SUV", "year": 2023, 
-       "car_make": car_make_instances[0],"dealerId": 1},
+       "car_make": car_make_instances[0], "dealerId": 1},
       {"name": "Qashqai", "type": "SUV", "year": 2023,
-       "car_make": car_make_instances[0],"dealerId": 1},
+       "car_make": car_make_instances[0], "dealerId": 1},
       {"name": "XTRAIL", "type": "SUV", "year": 2023,
-       "car_make": car_make_instances[0],"dealerId": 1},
+       "car_make": car_make_instances[0], "dealerId": 1},
       {"name": "A-Class", "type": "SUV", "year": 2023,
-       "car_make": car_make_instances[1],"dealerId": 1},
+       "car_make": car_make_instances[1], "dealerId": 1},
       {"name": "C-Class", "type": "SUV", "year": 2023,
-       "car_make": car_make_instances[1],"dealerId": 1},
+       "car_make": car_make_instances[1], "dealerId": 1},
       {"name": "E-Class", "type": "SUV", "year": 2023,
-       "car_make": car_make_instances[1],"dealerId": 1},
+       "car_make": car_make_instances[1], "dealerId": 1},
       {"name": "A4", "type": "SUV", "year": 2023,
        "car_make": car_make_instances[2],"dealerId": 1},
       {"name": "A5", "type": "SUV", "year": 2023,
@@ -46,10 +47,11 @@ def initiate():
       {"name": "Camry", "type": "Sedan", "year": 2023,
        "car_make": car_make_instances[4],"dealerId": 1},
       {"name": "Kluger", "type": "SUV", "year": 2023,
-       "car_make": car_make_instances[4],"dealerId": 1},
-        
+       "car_make": car_make_instances[4], "dealerId": 1},        
     ]
 
     for data in car_model_data:
-            CarModel.objects.create(name=data['name'], car_make=data['car_make'],
-                                    type=data['type'], year=data['year'], dealerId=data['dealerId'])
+        CarModel.objects.create(name=data['name'],
+                                car_make=data['car_make'], 
+                                type=data['type'], year=data['year'],
+                                dealerId=data['dealerId'])
